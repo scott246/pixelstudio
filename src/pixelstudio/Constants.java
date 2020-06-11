36150d83e1be6156abc8946382be8aa3d020b38f
Nathan Scott <ndscott32@gmail.com>
2020-06-11 16:17:29 -0400

    method refactoring

--- src/pixelstudio/Constants.java
+++ src/pixelstudio/Constants.java
@@ -12,9 +12,11 @@ public class Constants {
 	public static final Dimension INFO_DIMENSION = new Dimension(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
 	public static final Dimension CONTROL_DIMENSION = new Dimension(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
 	
-	public static final Color BACKGROUND_COLOR = new Color(64, 64, 64, 255);
-	public static final Color EDITOR_BACKGROUND_COLOR = new Color(0, 0, 255, 255);
-	public static final Color INFO_BACKGROUND_COLOR = new Color(0, 255, 0, 255);
-	public static final Color CONTROL_BACKGROUND_COLOR = new Color(255, 0, 0, 255);
+	public static final Color BACKGROUND_COLOR = new Color(32, 32, 32, 255);
+	public static final Color EDITOR_BACKGROUND_COLOR = new Color(64, 64, 64, 255);
+	public static final Color INFO_BACKGROUND_COLOR = new Color(64, 64, 64, 255);
+	public static final Color CONTROL_BACKGROUND_COLOR = new Color(64, 64, 64, 255);
+	public static final Color FOREGROUND_COLOR = new Color(255, 255, 255, 255);
+	public static final Color ACCENT_COLOR = new Color(255, 128, 0, 255);
 
 }
--- src/pixelstudio/window/Window.java
+++ src/pixelstudio/window/Window.java
@@ -2,43 +2,35 @@ package pixelstudio.window;
 
 import pixelstudio.Constants;
 
-import java.awt.Dimension;
-
 import javax.swing.BorderFactory;
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
 
-public class Main {
+public class Window {
     private static JFrame window = new JFrame("Pixel Studio");
+    
     private static JPanel windowContentPanel = new JPanel();
     private static JPanel editorPanel = new JPanel();
     private static JPanel infoControlPanel = new JPanel();
     private static JPanel infoPanel = new JPanel();
     private static JPanel controlPanel = new JPanel();
     
+    private static JLabel titleLabel = new JLabel();
+    
     private static Border panelBorder = BorderFactory.createMatteBorder(5, 5, 5, 5, Constants.BACKGROUND_COLOR);
 
-    public Main() {
-        editorPanel.setBackground(Constants.EDITOR_BACKGROUND_COLOR);
-    	editorPanel.setBorder(panelBorder);
-    	editorPanel.setPreferredSize(Constants.EDITOR_DIMENSION);
-
-        infoPanel.setBackground(Constants.INFO_BACKGROUND_COLOR);
-        infoPanel.setBorder(panelBorder);
-        infoPanel.setPreferredSize(Constants.INFO_DIMENSION);
-
-        controlPanel.setBackground(Constants.CONTROL_BACKGROUND_COLOR);
-        controlPanel.setBorder(panelBorder);
-        controlPanel.setPreferredSize(Constants.CONTROL_DIMENSION);
-
-        infoControlPanel.setLayout(new BoxLayout(infoControlPanel, BoxLayout.PAGE_AXIS));
-        infoControlPanel.add(infoPanel);
-        infoControlPanel.add(controlPanel);
+    public Window() {
+    	initializeWindowContentPanel();
+    }
+    
+    private static void initializeWindowContentPanel() {
+    	initializeEditorPanel();
+        initializeInfoControlPanel();
         
     	windowContentPanel.setBackground(Constants.BACKGROUND_COLOR);
         windowContentPanel.setLayout(new BoxLayout(windowContentPanel, BoxLayout.LINE_AXIS));
@@ -46,6 +38,36 @@ public class Main {
         windowContentPanel.add(editorPanel);
         windowContentPanel.add(infoControlPanel);
         windowContentPanel.setOpaque(true);
+    }
+    
+    private static void initializeEditorPanel() {
+    	editorPanel.setBackground(Constants.EDITOR_BACKGROUND_COLOR);
+    	editorPanel.setBorder(panelBorder);
+    	editorPanel.setPreferredSize(Constants.EDITOR_DIMENSION);
+    }
+    
+    private static void initializeInfoControlPanel() {
+    	initializeInfoPanel();
+        initializeControlPanel();
+        infoControlPanel.setLayout(new BoxLayout(infoControlPanel, BoxLayout.PAGE_AXIS));
+        infoControlPanel.add(infoPanel);
+        infoControlPanel.add(controlPanel);
+    }
+    
+    private static void initializeInfoPanel() {
+    	infoPanel.setBackground(Constants.INFO_BACKGROUND_COLOR);
+        infoPanel.setBorder(panelBorder);
+        infoPanel.setPreferredSize(Constants.INFO_DIMENSION);
+        titleLabel.setText("Pixel Studio");
+    	titleLabel.setSize(infoPanel.getWidth(), 100);
+    	titleLabel.setForeground(Constants.ACCENT_COLOR);
+    	infoPanel.add(titleLabel);
+    }
+    
+    private static void initializeControlPanel() {
+    	controlPanel.setBackground(Constants.CONTROL_BACKGROUND_COLOR);
+        controlPanel.setBorder(panelBorder);
+        controlPanel.setPreferredSize(Constants.CONTROL_DIMENSION);
     }
     
     private static void createAndShowGUI() {
@@ -58,7 +80,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Main();
+        new Window();
         
     	SwingUtilities.invokeLater(new Runnable() {
             public void run() {
