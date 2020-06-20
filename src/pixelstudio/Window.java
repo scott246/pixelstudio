0575d3ddc576bb33e766db8babbacb90c0811483
scott246 <ndscott32@gmail.com>
2020-06-20 18:26:29 -0400

    organized components and layout

--- src/pixelstudio/Window.java
+++ src/pixelstudio/Window.java
@@ -1,11 +1,18 @@
 package pixelstudio;
 
 import java.awt.BorderLayout;
-import java.awt.ComponentOrientation;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.Panel;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
@@ -17,15 +24,29 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class Window {
-    private static JFrame window = new JFrame("Pixel Studio");
-    
+    public static final int WINDOW_WIDTH = 900;
+	public static final int WINDOW_HEIGHT = 800;
+    public static final int BORDER_SIZE = 5;
+	public static final int PADDING = BORDER_SIZE * 2;
+	public static final Dimension WINDOW_DIMENSION = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT);
+	public static final Dimension TOOLBAR_DIMENSION = new Dimension(WINDOW_WIDTH, 30+PADDING);
+	public static final Dimension EDITOR_DIMENSION = new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT-(TOOLBAR_DIMENSION.height)*2);
+	
+	public static final Color BACKGROUND_COLOR = new Color(32, 32, 32, 255);
+	public static final Color EDITOR_BACKGROUND_COLOR = new Color(64, 64, 64, 255);
+	public static final Color TOOLBAR_BACKGROUND_COLOR = new Color(64, 64, 64, 255);
+	public static final Color FOREGROUND_COLOR = new Color(255, 255, 255, 255);
+	public static final Color ACCENT_COLOR = new Color(255, 128, 0, 255);
+	public static final Font LABEL_FONT = new Font(Font.DIALOG, Font.PLAIN, 12);
+	
+	private static JFrame window = new JFrame("Pixel Studio");
     private static JPanel windowContentPanel = new JPanel();
     
     private static JMenuBar menuBar = new JMenuBar();
@@ -64,23 +85,23 @@ public class Window {
     
     private static JPanel editorPanel = new JPanel();
     
-    private static JPanel infoPanel = new JPanel();
-    private static JLabel helpTextLabel = new JLabel("Help text");
-    private static JTextField titleTextField = new JTextField();
-    private static JButton saveButton = new JButton("Save");
-    
-    private static JPanel controlPanel = new JPanel();
-    private static JSpinner zoomSpinner = new JSpinner();
-    private static JPanel mapOverviewPanel = new JPanel();
-    private static JButton colorChooserButton = new JButton();
-    private static JPanel colorHistoryPanel = new JPanel();
+    private static JPanel upperToolbarPanel = new JPanel();
+    private static JButton chooseColorButton = new JButton("           ");
     private static JLabel selectedPaintColorLabel = new JLabel("#000000");
     private static JButton selectModeButton = new JButton("Select");
     private static JButton paintModeButton = new JButton("Paint");
-    private static JLabel selectedPixelColorLabel = new JLabel("#FFFFFF");
-    private static JLabel selectedPixelRangeLabel = new JLabel("0,0 : 1,0");
     
-    private static Border panelBorder = BorderFactory.createMatteBorder(5, 5, 5, 5, Constants.BACKGROUND_COLOR);
+    private static JPanel lowerToolbarPanel = new JPanel();
+    private static JLabel helpTextLabel = new JLabel("Help text");
+    private static JLabel titleLabel = new JLabel("Title");
+    private static JButton saveButton = new JButton("Save");
+    
+    private static Border panelBorder = BorderFactory.createMatteBorder(
+    		BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BACKGROUND_COLOR);
+    private static Border editorPanelBorder = BorderFactory.createMatteBorder(
+    		BORDER_SIZE, 0, BORDER_SIZE, 0, BACKGROUND_COLOR);
+    private static Border innerPanelBorder = BorderFactory.createEmptyBorder(
+    		BORDER_SIZE, BORDER_SIZE, BORDER_SIZE, BORDER_SIZE);
 
     public Window() {
     	initializeWindowContentPanel();
@@ -89,15 +110,15 @@ public class Window {
     private static void initializeWindowContentPanel() {
     	initializeMenuBar();
     	initializeEditorPanel();
-        initializeInfoPanel();
-        initializeControlPanel();
+        initializeUpperToolbarPanel();
+        initializeLowerToolbarPanel();
         
-    	windowContentPanel.setBackground(Constants.BACKGROUND_COLOR);
+    	windowContentPanel.setBackground(BACKGROUND_COLOR);
         windowContentPanel.setLayout(new BorderLayout());
     	windowContentPanel.setBorder(panelBorder);
         windowContentPanel.add(editorPanel, BorderLayout.CENTER);
-        windowContentPanel.add(infoPanel, BorderLayout.NORTH);
-        windowContentPanel.add(controlPanel, BorderLayout.EAST);
+        windowContentPanel.add(upperToolbarPanel, BorderLayout.NORTH);
+        windowContentPanel.add(lowerToolbarPanel, BorderLayout.SOUTH);
         windowContentPanel.setOpaque(true);
     }
     
@@ -143,43 +164,54 @@ public class Window {
     }
 
 	private static void initializeEditorPanel() {
-    	editorPanel.setBackground(Constants.EDITOR_BACKGROUND_COLOR);
-    	editorPanel.setBorder(panelBorder);
-    	editorPanel.setPreferredSize(Constants.EDITOR_DIMENSION);
+    	editorPanel.setBackground(EDITOR_BACKGROUND_COLOR);
+    	editorPanel.setBorder(editorPanelBorder);
+    	editorPanel.setPreferredSize(EDITOR_DIMENSION);
     }
     
-    private static void initializeInfoPanel() {
-    	infoPanel.setBackground(Constants.INFO_BACKGROUND_COLOR);
-        infoPanel.setBorder(panelBorder);
-        infoPanel.setPreferredSize(Constants.INFO_DIMENSION);
-        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.X_AXIS));
+    private static void initializeUpperToolbarPanel() {
+    	upperToolbarPanel.setBackground(TOOLBAR_BACKGROUND_COLOR);
+    	upperToolbarPanel.setBorder(innerPanelBorder);
+    	upperToolbarPanel.setPreferredSize(TOOLBAR_DIMENSION);
+        upperToolbarPanel.setLayout(new BoxLayout(upperToolbarPanel, BoxLayout.X_AXIS));
         
-        infoPanel.add(helpTextLabel);
-        infoPanel.add(titleTextField);
-	    infoPanel.add(saveButton);
+        upperToolbarPanel.add(selectModeButton);
+        upperToolbarPanel.add(paintModeButton);
+        
+        upperToolbarPanel.add(Box.createHorizontalGlue());
+
+        selectedPaintColorLabel.setFont(LABEL_FONT);
+        selectedPaintColorLabel.setForeground(FOREGROUND_COLOR);
+        upperToolbarPanel.add(selectedPaintColorLabel);
+        upperToolbarPanel.add(Box.createRigidArea(new Dimension(BORDER_SIZE, 0)));
+        upperToolbarPanel.add(chooseColorButton);
     }
     
-    private static void initializeControlPanel() {
-    	controlPanel.setBackground(Constants.CONTROL_BACKGROUND_COLOR);
-        controlPanel.setBorder(panelBorder);
-        controlPanel.setPreferredSize(Constants.CONTROL_DIMENSION);
-        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+    private static void initializeLowerToolbarPanel() {
+    	lowerToolbarPanel.setBackground(TOOLBAR_BACKGROUND_COLOR);
+    	lowerToolbarPanel.setBorder(innerPanelBorder);
+    	lowerToolbarPanel.setPreferredSize(TOOLBAR_DIMENSION);
+    	lowerToolbarPanel.setLayout(new BoxLayout(lowerToolbarPanel, BoxLayout.X_AXIS));
         
-        controlPanel.add(zoomSpinner);
-        controlPanel.add(mapOverviewPanel);
-        controlPanel.add(colorChooserButton);
-        controlPanel.add(colorHistoryPanel);
-        controlPanel.add(selectedPaintColorLabel);
-        controlPanel.add(selectModeButton);
-        controlPanel.add(paintModeButton);
-        controlPanel.add(selectedPixelColorLabel);
-        controlPanel.add(selectedPixelRangeLabel);
+        helpTextLabel.setFont(LABEL_FONT);
+        helpTextLabel.setForeground(FOREGROUND_COLOR);
+        lowerToolbarPanel.add(helpTextLabel);
+        
+        lowerToolbarPanel.add(Box.createHorizontalGlue());
+        
+        titleLabel.setFont(LABEL_FONT);
+        titleLabel.setForeground(FOREGROUND_COLOR);
+        titleLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        lowerToolbarPanel.add(titleLabel);
+        
+        lowerToolbarPanel.add(Box.createRigidArea(new Dimension(BORDER_SIZE, 0)));
+        lowerToolbarPanel.add(saveButton);
     }
     
     private static void initializeWindow() {
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setResizable(true);
-        window.setSize(Constants.WINDOW_DIMENSION);
+        window.setSize(WINDOW_DIMENSION);
         window.setContentPane(windowContentPanel);
         window.pack();
         window.setVisible(true);
